74e80b4a1ac037c11a2679f0ccc05f01475268a5
ReutMaor <reutm1999@gmail.com>
2024-12-08 20:16:25 +0200

    fourthCommit

--- ReutMaor.java
+++ ReutMaor.java
@@ -1,7 +1,7 @@
 public class ReutMaor{
   public static void main(String[] args){
     for(int i=1;i<=10;i++){
-      System.out.println("Reut Maor"+i);
+      System.out.println("Reut Maor After Change"+i);
     }
   }
 }
